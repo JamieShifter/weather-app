7a568b61d78aaf428402bb4e0d17fc56d4e2ec9c
JamieShifter <jakub.szczechowicz@gmail.com>
2022-11-04 12:40:26 +0100

    write tests for hello-world

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -4,4 +4,15 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+
+  test "should get index" do
+    get root_url
+    assert_response :success
+  end
+
+  test "should get hello world" do
+    get root_url
+    assert_select "h1", "Hello World!"
+  end
 end
+
